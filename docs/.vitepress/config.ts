1b8ac6191d9c56d7c2a477dba7284e2886644005
jonaslgtm <10245089@qq.com>
2025-11-06 16:48:53 +0800

    init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,103 @@
+import { basename } from 'node:path'
+import { defineConfig } from 'vitepress'
+import { La51Plugin } from 'vitepress-plugin-51la'
+import MarkdownPreview from 'vite-plugin-markdown-preview'
+
+import { head, nav, sidebar, algolia } from './configs'
+
+const APP_BASE_PATH = basename(process.env.APP_BASE_PATH || '')
+
+export default defineConfig({
+  outDir: '../dist',
+  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
+
+  lang: 'zh-CN',
+  title: '茂茂物语',
+  description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
+  head,
+
+  lastUpdated: true,
+  cleanUrls: true,
+  ignoreDeadLinks: 'localhostLinks',
+
+  /* markdown 配置 */
+  markdown: {
+    lineNumbers: true,
+    image: {
+      lazyLoading: true,
+    },
+  },
+
+  /* 主题配置 */
+  themeConfig: {
+    i18nRouting: false,
+
+    logo: '/logo.png',
+
+    nav,
+    sidebar,
+
+    /* 右侧大纲配置 */
+    outline: {
+      level: 'deep',
+      label: '目录',
+    },
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996' }],
+
+    footer: {
+      message: '如有转载或 CV 的请标注本站原文地址',
+      copyright: 'Copyright © 2019-present maomao',
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
+    },
+
+    /* Algolia DocSearch 配置 */
+    algolia,
+
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇',
+    },
+
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+
+    /*** 自定义配置 ***/
+    visitor: {
+      badgeId: 'jonaslgtm.notes',
+    },
+
+    comment: {
+      repo: 'jonaslgtm/mm-notes',
+      repoId: 'MDEwOlJlcG9zaXRvcnkxNTc0ODc5Mjg=',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOCWMTOM4CZ2rf',
+    },
+  },
+
+  /* 生成站点地图 */
+  sitemap: {
+    hostname: 'https://notes.fe-mm.com/',
+  },
+
+  vite: {
+    plugins: [MarkdownPreview(), La51Plugin({ id: '3LNfUkScYzEz6k4D', ck: '3LNfUkScYzEz6k4D' })],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
+  },
+})
