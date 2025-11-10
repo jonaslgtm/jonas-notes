5958cbe22fc8b2d9ee29cc575ebcbfeb37644732
jonaslgtm <10245089@qq.com>
2025-11-10 17:35:21 +0800

    更新 data.ts

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -119,13 +119,13 @@ export const NAV_DATA: NavData[] = [
     title: '前端学习资料',
     items: [
       {
-        icon: 'https://developer.mozilla.org/apple-touch-icon.6803c6f0.png',
+        icon: 'https://developer.mozilla.org/favicon.ico',
         title: 'MDN | Web 开发者指南',
         desc: 'Mozilla 的开发者平台，提供了大量关于 HTML、CSS 和 JavaScript 的详细文档以及广泛的 Web API 参考资',
         link: 'https://developer.mozilla.org/zh-CN'
       },
       {
-        icon: 'https://static.runoob.com/images/favicon.ico',
+        icon: 'https://www.runoob.com/favicon.ico',
         title: '菜鸟教程',
         desc: '学的不仅是技术，更是梦想！',
         link: 'https://www.runoob.com'
