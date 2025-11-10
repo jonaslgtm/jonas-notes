e040918383cd902ed389272c197fbf5b68d27042
jonaslgtm <10245089@qq.com>
2025-11-10 15:27:20 +0800

    更新 data.ts

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -689,6 +689,12 @@ export const NAV_DATA: NavData[] = [
         link: 'https://github.com'
       },
       {
+        title: '知乎',
+        icon: 'https://static.zhihu.com/heifetz/assets/apple-touch-icon-60.362a8eac.png',
+        desc: '中文互联网高质量的问答社区和创作者聚集的原创内容平台',
+        link: 'https://www.zhihu.com/'
+      },
+      {
         icon: 'https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png?v=c78bd457575a',
         title: 'Stack Overflow',
         desc: '全球最大的技术问答网站',
@@ -719,12 +725,6 @@ export const NAV_DATA: NavData[] = [
         desc: '博客园是一个面向开发者的知识分享社区',
         link: 'https://www.cnblogs.com'
       },
-      {
-        title: '知乎',
-        icon: 'https://static.zhihu.com/heifetz/assets/apple-touch-icon-60.362a8eac.png',
-        desc: '中文互联网高质量的问答社区和创作者聚集的原创内容平台',
-        link: 'https://juejin.cn'
-      }
     ]
   },
   {
